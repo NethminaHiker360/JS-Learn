12c7c847dfcfa4577b7cc6f3a8e37aa2de029b7c
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-08 10:33:36 +0530

    bmi cal

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-function lifeInWeeks(age){
-    var leftYears=90-age;
-    var leftWeeks=leftYears*52;
-    var leftMonths=leftYears*12;
-    var leftDays=leftYears*365;
-    console.log("You have "+leftYears+" Years Left, "+leftMonths+" Months Left, "+leftWeeks+" Weeks Left, "+leftDays+" Days Left")
+function bmiCal(weight,height){
+    return weight/((height)**2);
 }
 
-lifeInWeeks(20);
+console.log(bmiCal(50,1.25))
