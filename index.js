8fe5a98d4aa422f0b21387992fc5aca6553b57d1
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-08 11:53:55 +0530

    if else statment

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function randomPercentage(){
-    return Math.floor((Math.random()*100)+1);
+    var match= Math.floor((Math.random()*100)+1);
+    if(match>70){
+        return ( "Your Love Score is  : "+match+" , Very Good Match" )
+    }else{
+        return 'Your Love Score is  : ' + match;
+    }
 }
 
 console.log( randomPercentage());
