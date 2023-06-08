596282fa18173e7e452d6315b75f083c4299f13e
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-08 15:36:18 +0530

    fibonacchi series

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-var list=["nethu","ravindu","madhara","maleesha","mathindu"];
-console.log(list[Math.floor(Math.random()*(list.length)) ]+ ' is Going to Buy Lunch Today');
+var list = [];
+
+function fibonacchiSeries(n) {
+  var a = 0,
+    b = 1;
+  var count = 0;
+  while (count < n) {
+    if (count == 0) {
+      list.push(0);
+      count++;
+    } else if (count == 1) {
+      list.push(1);
+      count++;
+    } else {
+      list.push(a + b);
+      var c = b;
+      b = a + b;
+      a = c;
+      count++;
+    }
+  }
+  console.log(list);
+}
+
+fibonacchiSeries(8);
