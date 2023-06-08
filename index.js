4411c8a112a5efe1298f339d671f5282614ac113
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-08 15:10:50 +0530

    luncj challenge

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-function fizBuzz(){
-    for(var i = 1; i <= 100; i++){
-        if(i%3==0 && i%5==0){
-            console.log(i+" : FizzBuzz");
-        }else if(i%3==0){
-            console.log(i+" : Fizz");
-        }else if(i%5==0){
-            console.log(i+" : Buzz");
-        }else{
-            console.log(i);
-        }
-    }
-}
-
-fizBuzz();
+var list=["nethu","ravindu","madhara","maleesha","mathindu"];
+console.log(list[Math.floor(Math.random()*(list.length)) ]+ ' is Going to Buy Lunch Today');
