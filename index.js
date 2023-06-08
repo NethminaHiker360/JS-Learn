8ab6db1e72aa3f3de647034085639947cc3f4d79
Ravindu Nethmina <nethmina.kar@gmail.com>
2023-06-08 15:56:15 +0530

    fibonacchi series

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-var list = [];
-
 function fibonacchiSeries(n) {
-  var a = 0,
-    b = 1;
-  var count = 0;
-  while (count < n) {
-    if (count == 0) {
-      list.push(0);
-      count++;
-    } else if (count == 1) {
-      list.push(1);
-      count++;
-    } else {
-      list.push(a + b);
-      var c = b;
-      b = a + b;
-      a = c;
-      count++;
+    var list = [];
+    if(n==1){
+        list.push(0);
+    }else if(n==2){
+        list.push(0,1);
+    }else{
+        list.push(0,1)
+        var count=2;
+        while(count<n){
+            list.push(list[list.length-2]+list[list.length-1]);
+            count++;
+        }
     }
-  }
   console.log(list);
 }
 
-fibonacchiSeries(8);
+fibonacchiSeries(3);
